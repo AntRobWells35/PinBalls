0445d4157e17c94c7ffc662da0940ecfe6862762
ngomes007 <32779546+ngomes007@users.noreply.github.com>
2018-08-15 09:59:12 -0700

    test

---
+++ bowling/Assets/Scripts/BallControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Fixedupdate()
+    {
+        float mfw = Input.GetAxis("Horizontal");
+        float mside = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(mfw, 0.0F, mside);
+        rb.AddForce(movement * speed); 
+    }
+}
