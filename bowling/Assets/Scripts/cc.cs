0c3d39f157eab0a666d36942ebfc7cf885dceca9
ngomes007 <32779546+ngomes007@users.noreply.github.com>
2018-08-25 13:17:43 -0700

    camera

--- bowling/Assets/Scripts/cc.cs
+++ bowling/Assets/Scripts/cc.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cc : MonoBehaviour {
-	public GameObject player;
+    public Transform playertransform; 
+    public bool AroundPlayer;
+    public float Rotspeed = 5.0f; 
+	private Vector3 _cameraOffset;
+    [Range(0.0f, 1.0f)]
+    public float SmoothFact = 0.5f;
+    public bool LookAtPlayer = false; 
 
-	private Vector3 offset; 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;  
+        _cameraOffset = transform.position - playertransform.position;  
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = player.transform.position + offset; 
-	}
+        if (AroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * Rotspeed, Vector3.up);
+            _cameraOffset = camTurnAngle * _cameraOffset; 
+        }
+        Vector3 newPos = playertransform.position + _cameraOffset; 
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFact);
+        if (LookAtPlayer || AroundPlayer)
+            transform.LookAt(playertransform); 
+
+    }
 }
