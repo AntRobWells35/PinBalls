20c9ed5af465daeae6e30bdc672afa5a27cb7987
ngomes007 <32779546+ngomes007@users.noreply.github.com>
2018-08-24 19:49:23 -0700

    BallControl

--- bowling/Assets/Scripts/BallControl.cs
+++ bowling/Assets/Scripts/BallControl.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallControl : MonoBehaviour {
-    public float speed;  
- 
-        
-    void Uupdate()
-    {
-        float mfw = Input.GetAxis("Horizontal");
-        float mside = Input.GetAxis("Vertical");
+    public float speed = 10.0F; 
+     	private Rigidbody Rb;
+    public float rotationSpeed = 75.0f;
 
-        Rigidbody body = GetComponent<Rigidbody>();
-body.AddTorque(new Vector3(mfw, 0, mside) * speed * Time.deltaTime); 
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; 
     }
-}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Mouse Y") * speed;
+
+
+        float rotation = Input.GetAxis("Mouse X") * rotationSpeed;
+
+        rotation *= Time.deltaTime;
+
+        Vector3 movement = new Vector3(rotation, 0.0F, translation);
+        Rb.AddForce(movement * speed);
+
+        transform.Rotate(0, rotation, 0);
+        if (Input.GetKeyDown("escaps"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+  
+    }
